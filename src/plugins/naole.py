ec0562b681a304e3a17d7374d7497766e23f4055
vol009 <vol009@126.com>
2025-03-26 19:45:25 +0800

    Removed the broke requirements.txt add functions

---
+++ src/plugins/naole.py
@@ -0,0 +1,10 @@
+from nonebot import on_regex
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, Message # type: ignore
+
+nao = on_regex(pattern=r'^闹了$',priority=1)
+
+@nao.handle()
+async def naoL (bot: Bot, event: GroupMessageEvent, state: T_State):
+    msg = f"这里应该是输出一张闹了对应的图片 但我还没写完 就先占个位置（"
+    await nao.finish(message=Message(msg))
