f45c3aebb40bac0d74d56370f3082d78446024ca
vol009 <vol009@126.com>
2025-03-28 20:05:51 +0800

    更新了welcome.py和naole.py

--- src/plugins/naole.py
+++ src/plugins/naole.py
@@ -1,11 +1,23 @@
 from nonebot import on_regex
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, Message  # type: ignore
+import os, logging
 
 nao = on_regex(pattern=r"^闹了$", priority=1)
 
 
 @nao.handle()
 async def naoL(bot: Bot, event: GroupMessageEvent, state: T_State):
-    msg = f"这里应该是输出一张闹了对应的图片 但我还没写完 就先占个位置（"
-    await nao.finish(message=Message(msg))
+    if str(event.group_id) == "996101999" or event.group_id == "225173408":
+        1145141919810
+        try:
+            file_path = os.path.join(os.path.dirname(__file__), "naole.png")
+            logging.info("sending image..")
+            await bot.call_api("send_msg", message_type="group", group_id=event.group_id, message = {"type": "image", "file": file_path})
+            # 上面这行代码是错的 我等下改
+        except Exception as e:
+            logging.error(f"Error: {e}")
+        # msg = f"这里应该是输出一张闹了对应的图片 但我还没写完 就先占个位置（"
+        await nao.finish()
+    else:
+        await nao.finish()
