f25e1560f588ed5cb38bdadb10ec46b4ed920e0a
interfacew <interface@163.com>
2025-03-28 20:19:32 +0800

    添加对jm插件的更多config，修改了默认存储位置

--- src/plugins/jm/config.py
+++ src/plugins/jm/config.py
@@ -1,3 +1,6 @@
 class Config:
     allow_group = False
     allow_private = True
+    img_path = "./cache/jm/src/"
+    pdf_path = "./cache/jm/pdf/"
+    download_thread = 16
--- src/plugins/jm/jmdownload.py
+++ src/plugins/jm/jmdownload.py
@@ -1,42 +1,44 @@
+from typing import Tuple
 import jmcomic
 import os
 import asyncio
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 import jmcomic.jm_exception
+from .config import Config
 
 
 def jm_init():
     global option
     option = jmcomic.create_option_by_str(
-        """
+        f"""
 download:
   cache: true
   image:
     suffix: .png
   threading:
-    image: 16
+    image: {Config.download_thread}
 
 dir_rule:
-  base_dir: ./jmcache/src/
+  base_dir: {Config.img_path}
   rule: Bd_Aid
 
 plugins:
   after_album:
     - plugin: img2pdf
       kwargs:
-        pdf_dir: ./jmcache/pdf/
+        pdf_dir: {Config.pdf_path}
         filename_rule: Aid
         delete_original_file: false
 """
     )
 
 
-async def jm_download(code: str) -> tuple[int, str]:
+async def jm_download(code: str) -> Tuple[int, str]:
     if not code.isdecimal():
         return -1, None
 
-    pdf_path = os.path.abspath(os.path.join("./jmcache/pdf/", f"{code}.pdf"))
+    pdf_path = os.path.abspath(os.path.join(Config.pdf_path, f"{code}.pdf"))
 
     if os.path.exists(pdf_path):
         return 0, pdf_path
@@ -54,5 +56,5 @@ async def jm_download(code: str) -> tuple[int, str]:
         if len(downloader.download_failed_list) != 0:
             os.remove(pdf_path)
             return -1, "未成功下载"
-    shutil.rmtree(os.path.abspath(f"./jmcache/src/{code}"))
+    shutil.rmtree(os.path.abspath(os.path.join(Config.img_path, code)))
     return 0, pdf_path
