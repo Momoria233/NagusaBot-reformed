f52d43ee642d7bafd049ffbf2cc7d1b91aea2113
vol009 <vol009@126.com>
2025-03-29 00:35:21 +0800

    在react中更新了内置cd

--- src/plugins/react/__init__.py
+++ src/plugins/react/__init__.py
@@ -1,4 +1,4 @@
-import random, os
+import random, os, time
 from nonebot import on_notice, on_regex
 from nonebot.typing import T_State
 from nonebot import logger
@@ -8,14 +8,27 @@ from .config import Config
 
 EatL = on_regex(pattern=r"^吃饭$", priority=1)
 
+cooldown_tracker = {}
+cooldown_period = 30
 
 @EatL.handle()
 async def Eat(bot: Bot, event: GroupMessageEvent, state: T_State):
+    user_id = event.get_user_id()
+    current_time = time.time()
+    at = MessageSegment.at(event.get_user_id())
+
+    if user_id in cooldown_tracker:
+        last_used = cooldown_tracker[user_id]
+        if current_time - last_used < cooldown_period:
+            remaining_time = cooldown_period - (current_time - last_used)
+            await EatL.finish()
+    cooldown_tracker[user_id] = current_time
+
     if not Config.activate_eat:
         await EatL.finish()
     randList = Config.food + Config.stu
     msg = f"吃到了{random.choice(randList)}。"
-    await EatL.finish(message=Message(msg))
+    await EatL.finish(message=Message([at,msg]))
 
 
 pokeReact = on_notice()
@@ -48,9 +61,20 @@ nao = on_regex(pattern=r"^闹了$", priority=1)
 
 @nao.handle()
 async def naoL(bot: Bot, event: GroupMessageEvent, state: T_State):
+    user_id = event.get_user_id()
+    current_time = time.time()
+    at = MessageSegment.at(event.get_user_id())
+
+    if user_id in cooldown_tracker:
+        last_used = cooldown_tracker[user_id]
+        if current_time - last_used < cooldown_period:
+            remaining_time = cooldown_period - (current_time - last_used)
+            await EatL.finish()
+    cooldown_tracker[user_id] = current_time
+
     if not Config.activate_nao:
         await nao.finish()
     if event.group_id == 996101999 or event.group_id == 225173408:
-        await nao.finish(MessageSegment.image(os.path.join(os.path.dirname(os.path.abspath(__file__)), "naole.png")))
+        await nao.finish(message = Message([at,MessageSegment.image(os.path.join(os.path.dirname(os.path.abspath(__file__)), "naole.png"))]))
     else:
         await nao.finish()
