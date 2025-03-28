dc368e9ea2facfd975920c6698e420f94a9ff505
interfacew <interface@163.com>
2025-03-28 20:20:26 +0800

    初步实现生日播报插件

---
+++ src/plugins/birthday/__init__.py
@@ -0,0 +1,95 @@
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
+from typing import List
+import requests
+import asyncio
+import pytz
+import json
+import os
+from nonebot.adapters.onebot.v11 import Bot
+from nonebot import get_driver
+from nonebot import get_bot
+from nonebot import require
+from nonebot import logger
+
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
+from .config import Config
+
+
+async def update_config():
+    logger.info("Updating students data...")
+    try:
+        with ThreadPoolExecutor() as executor:
+            loop = asyncio.get_running_loop()
+            res = await loop.run_in_executor(executor, requests.get, Config.data_url)
+        res.raise_for_status()
+        with open(Config.data_path, "wb") as file:
+            file.write(res.content)
+        logger.info("Data Updated!")
+    except:
+        if not os.path.exists(Config.data_path):
+            logger.error("Data update failed, cache not found.")
+            return False
+        logger.warning("Data update failed, using cache instead.")
+    return True
+
+
+driver = get_driver()
+
+
+@driver.on_startup
+async def init_func():
+    with ThreadPoolExecutor() as executor:
+        await update_config(executor)
+    global start_date, tz
+    tz = pytz.timezone("Asia/Shanghai")
+    start_date: datetime = datetime.now(tz).replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+    logger.info(f"Next action will occur at {start_date.strftime("%a %b %d %Y %H:%M:%S GMT%z (%Z)")}")
+
+
+def get_birthday(now: datetime) -> List[str]:
+
+    now_mounth: int = now.month
+    now_day: int = now.day
+
+    students: List[str] = []
+    with open(Config.data_path, "r", encoding="utf-8") as file:
+        data = json.load(file.read())
+    for student in data:
+        birthday: str = student["BirthDay"]
+        name: str = student["PersonalName"]
+        try:
+            month: int = int(birthday.split("/")[0])
+            day: int = int(birthday.split("/")[1])
+        except (ValueError, IndexError):
+            logger.warning(f"An error occurred while converting {name}'s birthday, so the student will not be counted in the data")
+            continue
+        if day == now_day and month == now_mounth:
+            students.append(name)
+
+    return students
+
+
+@scheduler.scheduled_job("interval", days=1, start_date=start_date, id="job_birthday")
+async def report_birthday():
+    global tz
+    now: datetime = datetime.now(tz)
+
+    res = await update_config()
+    if not res:
+        logger.error("Skip this action")
+        return
+
+    with ThreadPoolExecutor() as executor:
+        loop = asyncio.get_running_loop()
+        students = await loop.run_in_executor(executor, get_birthday, now)
+    logger.info(f"Today's birthday girl: {students}")
+
+    bot = get_bot()
+    assert isinstance(bot, Bot)
+    for id in Config.target_group_id:
+        bot.send_group_msg(group_id=id, message="......")  # TODO 生日祝福消息格式
+
+    next_date: datetime = now.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+    logger.info(f"Next action will occur at {next_date.strftime("%a %b %d %Y %H:%M:%S GMT%z (%Z)")}")
---
+++ src/plugins/birthday/config.py
@@ -0,0 +1,4 @@
+class Config:
+    data_url = "https://github.com/SchaleDB/SchaleDB/blob/main/data/cn/students.json"
+    data_path = "./cache/birthday/students.json"
+    target_group_id = []
