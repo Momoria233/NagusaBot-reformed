7b75b58f4a37b48ec441fef1d018cf6da27466ce
vol009 <vol009@126.com>
2025-05-14 02:54:03 +0800

    updated autoGroupAcception

--- src/plugins/autoGroupAcception/__init__.py
+++ src/plugins/autoGroupAcception/__init__.py
@@ -8,19 +8,21 @@ import json, os
 
 assets_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets")
 
+with open(os.path.join(assets_dir,"replacement.json"), 'r', encoding='utf-8') as f:
+            checkData = json.load(f)
+
 def check_stu_name(input_name):
-    with open(os.path.join(assets_dir,"replacement.json"), 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            for item in data:
+            for item in checkData:
                 if (input_name == item.get("FullName") or
                     input_name == item.get("FamilyName") or
-                    input_name == item.get("PersonalName")):
+                    input_name == item.get("PersonalName") or
+                    item.get("FullName") in input_name):
                     return True
                 # else:
                 #     logger.warning(f"{input_name} not in {item.get('FullName')}, {item.get('FamilyName')}, {item.get('PersonalName')}")
             return False
 
-GroupRequest = on_request()
+GroupRequest = on_request(priority=1)
 @GroupRequest.handle()
 async def handle_group_request(bot:Bot, event: GroupRequestEvent):
     logger.info(f"[autoGroupAcception] Group {event.group_id} request from {event.user_id}")
@@ -31,14 +33,13 @@ async def handle_group_request(bot:Bot, event: GroupRequestEvent):
     answer = event.comment.split("答案：", 1)[1] if "答案：" in event.comment else ""
     logger.info(answer)
     if check_stu_name(answer):
-        # await bot.call_api(
-        #     "set_group_request",
-        #     flag=event.flag,
-        #     approve=True,
-        # )
         await event.approve(bot)
-        logger.info(f"[autoGroupAcception] Group {event.group_id} request from {event.user_id} approved")
+        msg = f"[autoGroupAcception] Group {event.group_id} request from {event.user_id} approved"
+        logger.info(msg)
+        bot.send_private_msg(user_id=2447209382,message=msg)
         await GroupRequest.finish()
     else:
-        logger.info(f"[autoGroupAcception] Group {event.group_id} request from {event.user_id} rejected")
+        msg = f"[autoGroupAcception] Group {event.group_id} request from {event.user_id} rejected, answer: {answer}"
+        logger.info(msg)
+        bot.send_private_msg(user_id=2447209382,message=msg)
         await GroupRequest.finish()
